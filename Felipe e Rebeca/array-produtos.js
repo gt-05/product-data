95a161ade363990a0181265f99ce9e6f81ae9d81
unknown <FELIPE0571@DIGITALCOLLEGE.LOCAL>
2024-10-02 09:41:17 -0300

    criação do array

---
+++ Felipe e Rebeca/array-produtos.js
@@ -0,0 +1,84 @@
+let produtos = [
+  {
+        "id": 1,
+        "name": "Smartphone XYZ",
+        "price": 999.99,
+        "discountPrice": 899.99,
+        "discount": 10.0,
+        "category": "Eletrônicos"
+    },
+    {
+        "id": 2,
+        "name": "Laptop ABC",
+        "price": 1499.99,
+        "discountPrice": 1299.99,
+        "discount": 13.33,
+        "category": "Eletrônicos"
+    },
+    {
+        "id": 3,
+        "name": "Cafeteira Gourmet",
+        "price": 299.99,
+        "discountPrice": 249.99,
+        "discount": 16.67,
+        "category": "Eletrodomésticos"
+    },
+    {
+        "id": 4,
+        "name": "Fone de Ouvido Bluetooth",
+        "price": 199.99,
+        "discountPrice": 159.99,
+        "discount": 20.0,
+        "category": "Acessórios"
+    },
+    {
+        "id": 5,
+        "name": "Monitor Ultra HD",
+        "price": 799.99,
+        "discountPrice": 699.99,
+        "discount": 12.5,
+        "category": "Eletrônicos"
+    },
+    {
+        "id": 6,
+        "name": "Tênis Esportivo",
+        "price": 129.99,
+        "discountPrice": 99.99,
+        "discount": 23.08,
+        "category": "Moda"
+    },
+    {
+        "id": 7,
+        "name": "Relógio Smart",
+        "price": 299.99,
+        "discountPrice": 249.99,
+        "discount": 16.67,
+        "category": "Acessórios"
+    },
+    {
+        "id": 8,
+        "name": "Batedeira Profissional",
+        "price": 399.99,
+        "discountPrice": 349.99,
+        "discount": 12.5,
+        "category": "Eletrodomésticos"
+    },
+    {
+        "id": 9,
+        "name": "Cadeira Gamer",
+        "price": 599.99,
+        "discountPrice": 499.99,
+        "discount": 16.67,
+        "category": "Móveis"
+    },
+    {
+        "id": 10,
+        "name": "Console de Videogame",
+        "price": 399.99,
+        "discountPrice": 349.99,
+        "discount": 12.5,
+        "category": "Eletrônicos"
+    }
+]
+
+    
