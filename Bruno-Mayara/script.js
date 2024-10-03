a1a9016d7c4096511abb670caa09c910a7bcc6c7
mayara <mayarabonates@gmai.com>
2024-10-03 08:41:47 -0300

    atualizando

--- Bruno-Mayara/script.js
+++ Bruno-Mayara/script.js
@@ -80,3 +80,115 @@ const produtos = [
         "category": "Perfumes"
     }
 ]
+
+//2
+function aplicarDesconto(produtos) {
+    const produtosComDesconto = [];
+
+    for (let i = 0; i < produtos.length; i++) {
+        const produto = produtos[i];
+        const descontoAplicado = produto.price * (produto.discount / 100);
+        const novoPreco = produto.price - descontoAplicado;
+
+        produtosComDesconto.push({
+            id: produto.id,
+            name: produto.name,
+            price: produto.price,
+            discount: produto.discount,
+            category: produto.category,
+            discountPrice: novoPreco.toFixed(2)
+        });
+    }
+
+    return produtosComDesconto;
+}
+const produtosComDesconto = aplicarDesconto(produtos);
+console.log(produtosComDesconto);
+
+//3
+function recebeCategoria(produtos, category) {
+    
+    const resultado = [];
+    for(let i = 0; i < produtos.length; i++){
+        if (produtos[i].category === category) {
+            resultado.push(produtos[i]);
+    } 
+}
+    return resultado;
+}
+const teste = recebeCategoria(produtos, "Calçados")
+console.log(teste);
+
+//4
+function buscarProdutosPorNome(produtos, termo) {
+    const produtosEncontrados = [];
+
+    for (let i = 0; i < produtos.length; i++) {
+        const produto = produtos[i];
+
+        if (produto.name.toLowerCase().includes(termo.toLowerCase())) {
+            produtosEncontrados.push(produto);
+        }
+    }
+
+    return produtosEncontrados;
+}
+
+const resultado = buscarProdutosPorNome(produtos, "coca");
+console.log(resultado);
+
+//5
+function totalPreco(produtos) {
+    let total = 0;
+    for (let i = 0; i < produtos.length; i++) {
+        total += produtos[i].price;
+    }
+    return total;
+}
+
+console.log("Total de Preços:", totalPreco(produtos));
+
+//6
+function produtosOrdenadosPorPreco(produtos) {
+    const produtosOrdenados = [...produtos]; 
+    const n = produtosOrdenados.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - 1; j++) {
+            if (produtosOrdenados[j].price > produtosOrdenados[j + 1].price) {
+                
+                const temp = produtosOrdenados[j];
+                produtosOrdenados[j] = produtosOrdenados[j + 1];
+                produtosOrdenados[j + 1] = temp;
+            }
+        }
+    }
+    return produtosOrdenados;
+}
+
+console.log("Produtos Ordenados por Preço:", produtosOrdenadosPorPreco(produtos));
+//
+//7
+function temDesconto(produtos) {
+    for (let i = 0; i < produtos.length; i++) {
+        if (produtos[i].discount > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log("Tem Desconto?", temDesconto(produtos));
+
+//8
+function todosPrecosAcimaDe100(produtos) {
+    for (let i = 0; i < produtos.length; i++) {
+        if (produtos[i].price <= 100) {
+            return false; 
+        }
+    }
+    return true; 
+}
+
+console.log("Todos Preços Acima de 100?", todosPrecosAcimaDe100(produtos));
+
+//
